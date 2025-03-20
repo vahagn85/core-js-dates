24750de0541d854b9fd95c201ebab4346e2fab5a
vahagn85 <v.ulikhanyan@yandex.ru>
2025-03-20 23:19:37 +0400

    feat: implement getTime

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -31,8 +31,9 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const addZeroToTime = (time) => String(time).padStart(2, '0');
+  return `${addZeroToTime(date.getHours())}:${addZeroToTime(date.getMinutes())}:${addZeroToTime(date.getSeconds())}`;
 }
 
 /**
