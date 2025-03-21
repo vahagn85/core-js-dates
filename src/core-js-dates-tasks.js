16a4f59452fd958b5a53f3680f21d3ccc1af80ca
vahagn85 <v.ulikhanyan@yandex.ru>
2025-03-21 13:40:38 +0400

    feat: implement getNextFriday

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -72,8 +72,10 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const fridayDay = (5 - date.getUTCDay() + 7) % 7 || 7;
+  date.setUTCDate(date.getUTCDate() + fridayDay);
+  return date;
 }
 
 /**
